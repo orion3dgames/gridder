4ef19f2f109c452b0d9272f0ad78882649018406
Orion <orion@splashbox.com.au>
2013-07-25 19:01:39 +1000

    Options to choose the number of columns and gutter width added

--- js/gridder.js
+++ js/gridder.js
@@ -8,9 +8,26 @@
 		var gridderprev = $('<div class="gridder-prev"></div>');
 		var griddernext = $('<div class="gridder-next"></div>');
 		
+		var columns = 4;
+		var gutter = 1;
+		
 		gridder.append(gridderclose,gridderprev,griddernext);
 		
 		return this.each(function() {
+	
+	
+			$('.gridder-list').each(function(key, value){
+				
+				/* Initialize a few variables */
+				column_width = 100 / columns - (gutter) + (gutter / columns) ;
+				result = column_width.toFixed(3);
+				
+				$(this).css('width', result+"%");
+				
+				if((key+1)%columns){
+					$(this).css('margin-right', gutter+"%");
+				}
+			});
 			
 			
 			$('.gridder-list').click(function(e) {
@@ -31,22 +48,29 @@
 					$('.gridder-show').append("<div class=content>"+currentcontent+"</div>");
 					
 				} else {
+					/* Adds the Expander bloc*/
+					mybloc = gridder.insertAfter(this).css('display', 'block');
 					
-					gridder.insertAfter(this).css('display', 'block');
+					/* Get the current title */
+					
+					/* Adds the content */
 					$('.gridder-show').append("<div class=content>"+currentcontent+"</div>");
 					
+					/* Make sure the correct bloc is active*/
 					if(!$(this).hasClass('imactive')){ 
 					$('.imactive').removeClass('imactive');
 					$(this).addClass("imactive");
 					}
 				}
 				
+				/* Scrolls to the current row */
 				$('html, body').animate({
 					scrollTop: $(this).position().top
-				}, 'medium');
+				}, 0);
 			
 			});
-						
+			
+			/* Close */
 			$('.gridder').on('click', '.gridder-close', function() {
 				$('.imactive').removeClass('imactive');
 				$('.gridder-show .content').remove();
@@ -55,10 +79,12 @@
 				
 			});
 			
+			/* Next */
 			$('.gridder').on('click', '.gridder-next', function() {				
 				$(this).parent().next().trigger('click');
 			});
 			
+			/* Previous */
 			$('.gridder').on('click', '.gridder-prev', function() {				
 				$(this).parent().prev().prev().trigger('click');
 			});
