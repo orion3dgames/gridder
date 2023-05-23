2fcec589e621d90cdce2c189b7d7984cce8eae52
Orion Gunning <orion.gunning@gmail.com>
2023-05-23 10:29:13 +1000

    updating dependancies

--- src/jquery.gridder.js
+++ src/jquery.gridder.js
@@ -1,14 +1,5 @@
 (function(factory) {
-    if (typeof define === "function" && define.amd) {
-      // AMD. Register as an anonymous module depending on jQuery.
-      define(["jquery"], factory);
-    } else if (typeof exports === "object") {
-      // Node/CommonJS
-      module.exports = factory(require("jquery"));
-    } else {
-      // No AMD. Register plugin with global jQuery object.
-      factory(jQuery);
-    }
+    factory(jQuery);
   })(function($) {
 
     /* Custom Easing */
